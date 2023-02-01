d6f943bcc568510d55d46e2291ea51f08350c4cf
Khaledtf Labeb <khaledtf19@gmail.com>
2023-02-01 20:13:48 +0200

    update logo

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,22 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        <img
+          src="https://www.propelleraero.com/wp-content/uploads/2021/05/Vector.svg"
+          className="App-logo"
+          alt="logo"
+        />
         <p>
           Edit <code>src/App.tsx</code> and save to reload.
         </p>
         <a
           className="App-link"
-          href="https://reactjs.org"
+          href="https://www.propelleraero.com/dirtmate/"
           target="_blank"
           rel="noopener noreferrer"
         >
